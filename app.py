0b806d0b1e8d263c88d1659d3094ea8e9efe63a8
AfroNinjaaaaaaaa <m1105.108.megaman@gmail.com>
2023-01-22 23:00:36 +0100

    Added textarea & fixed qrcode generation

--- app.py
+++ app.py
@@ -12,16 +12,6 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/generate_qrcode', methods=['POST'])
-def generate_qrcode():
-    url = request.form['url']
-    img = qrcode.make(url)
-    bio = BytesIO()
-    img.save(bio, format='png')
-    bio.seek(0)
-    return send_file(bio, mimetype='image/png')
-
-
 @app.route('/generate_url', methods=['POST'])
 def generate_url():
     file = request.files['qr_file']
@@ -29,8 +19,19 @@ def generate_url():
     barcodes = decode(image)
     for barcode in barcodes:
         (barcode_data, barcode_type) = barcode.data, barcode.type
-        return jsonify({"url": barcode_data.decode()})
-    return jsonify({"error": "No QR code found in the image"})
+        return render_template('index.html', qr_content=barcode_data.decode())
+    return render_template('index.html', qr_content={"error": "No QR code found in the image"})
+
+
+@app.route('/generate_qrcode', methods=['POST'])
+def generate_qrcode():
+
+    url = request.form['text_qr_content']
+    img = qrcode.make(url)
+    bio = BytesIO()
+    img.save(bio, format='png')
+    bio.seek(0)
+    return send_file(bio, mimetype='image/png')
 
 
 if __name__ == '__main__':
