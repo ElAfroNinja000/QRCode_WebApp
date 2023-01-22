f20d19db9bd41b7ece13637dff4758878a8da28f
AfroNinjaaaaaaaa <m1105.108.megaman@gmail.com>
2023-01-22 18:59:15 +0100

    Added URL generation from QR code.

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, render_template, send_file
+from flask import Flask, request, render_template, send_file, jsonify
+from pyzbar.pyzbar import decode
+from PIL import Image
 import qrcode
 from io import BytesIO
 
@@ -10,14 +12,25 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/generate', methods=['POST'])
-def generate():
+@app.route('/generate_qrcode', methods=['POST'])
+def generate_qrcode():
     url = request.form['url']
     img = qrcode.make(url)
     bio = BytesIO()
     img.save(bio, format='png')
     bio.seek(0)
     return send_file(bio, mimetype='image/png')
+
+
+@app.route('/generate_url', methods=['POST'])
+def generate_url():
+    file = request.files['qr_file']
+    image = Image.open(file)
+    barcodes = decode(image)
+    for barcode in barcodes:
+        (barcode_data, barcode_type) = barcode.data, barcode.type
+        return jsonify({"url": barcode_data.decode()})
+    return jsonify({"error": "No QR code found in the image"})
 
 
 if __name__ == '__main__':
