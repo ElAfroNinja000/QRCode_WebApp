e375dbc7ceb05e183fd6d7fafa5c83fe1c292fba
AfroNinjaaaaaaaa <m1105.108.megaman@gmail.com>
2023-01-25 00:54:08 +0100

    Separated qr display and qr download Debug

--- app.py
+++ app.py
@@ -2,14 +2,8 @@ from flask import Flask, Response, request, render_template, send_file, jsonify
 from pyzbar.pyzbar import decode
 from PIL import Image
 import qrcode
-from io import BytesIO
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 
 @app.route('/generate_url', methods=['POST'])
@@ -18,22 +12,31 @@ def generate_url():
     image = Image.open(file)
     barcodes = decode(image)
     for barcode in barcodes:
-        (barcode_data, barcode_type) = barcode.data, barcode.type
-        return render_template('index.html', qr_content=barcode_data.decode())
-    return render_template('index.html', qr_content={"error": "No QR code found in the image"})
+        barcode_data = barcode.data
+        print(type(barcode_data.decode()))
+        return render_template('index.html', url=barcode_data.decode())
+    return render_template('index.html', url={"error": "No QR code found in the image"})
 
 
-@app.route('/generate_qrcode', methods=['POST'])
-def generate_qrcode():
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    url = ""
+    if request.method == 'POST':
+        url = request.form['qr_content']
+        img = qrcode.make(url)
+        img.save('static/generated_qr.png')
+    return render_template('index.html', url=url)
 
-    url = request.form['text_qr_content']
-    img = qrcode.make(url)
-    bio = BytesIO()
-    bio.seek(0)
-    response = Response(content_type="image/jpeg")
-    img.save(bio, format='jpeg')
-    response.headers["Content-Disposition"] = "attachment; filename=image.jpg"
-    #return send_file(bio, mimetype='image/png')
+
+@app.route("/qr_code")
+def qr_code():
+    return send_file('static/generated_qr.png', mimetype='image/png')
+
+
+@app.route('/download_qrcode', methods=['POST'])
+def download_qrcode():
+    response = Response(content_type="image/png")
+    response.headers["Content-Disposition"] = "attachment; filename=qr_code.png"
     return response
 
 
